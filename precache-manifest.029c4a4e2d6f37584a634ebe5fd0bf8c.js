97f7e0fefa5c3ad52ab21c72cac1973628a6a820
Oscar Stanchi <indirivacua@hotmail.com>
2022-04-30 13:10:32 -0300

    Updates

---
+++ precache-manifest.029c4a4e2d6f37584a634ebe5fd0bf8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d4a66dffa52e065c6547c291a144804",
+    "url": "/pose-mediapipe/index.html"
+  },
+  {
+    "revision": "312127678df64f29ad2b",
+    "url": "/pose-mediapipe/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "93abf8fb412f3ac2a238",
+    "url": "/pose-mediapipe/static/js/2.67f35ff0.chunk.js"
+  },
+  {
+    "revision": "aa9fa882168d1a38473469bc4877196d",
+    "url": "/pose-mediapipe/static/js/2.67f35ff0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "312127678df64f29ad2b",
+    "url": "/pose-mediapipe/static/js/main.5019bf28.chunk.js"
+  },
+  {
+    "revision": "916e2dde53b3e6209588",
+    "url": "/pose-mediapipe/static/js/runtime-main.c7bc170f.js"
+  }
+]);
